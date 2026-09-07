30888795b25aa9bc0ea4bf047922ccd739c6a279
Farhan-Tanvir00 <farhantanvir139@gmail.com>
2026-09-07 17:05:52 +0600

    Add project files.

---
+++ src/RabbitMqConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+internal class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("RabbitMQ Consumer 1");
+
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost"
+        };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        await using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(
+            queue: "hello",
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        consumer.ReceivedAsync += async (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = System.Text.Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received: {message}");
+            await Task.CompletedTask;
+        };
+
+        await channel.BasicConsumeAsync(
+            queue: "hello",
+            autoAck: true,
+            consumer: consumer);
+
+        Console.WriteLine("Waiting for messages...");
+        Console.ReadLine();
+    }
+}
---
+++ src/RabbitMqPublisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("RabbitMQ Producer");
+
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost"
+        };
+
+        using var connection = await factory.CreateConnectionAsync();
+
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(
+            queue: "hello",
+            durable: true,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        string message = "Hello RabbitMQ";
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(
+            exchange: "",
+            routingKey: "hello",
+            mandatory: false,
+            basicProperties: new BasicProperties(),
+            body: body);
+
+        Console.WriteLine("Message Sent");
+    }
+}
